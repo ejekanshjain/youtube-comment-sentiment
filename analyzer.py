aa9ead9e063b78a2d8126f3975e8080912d8a7d1
Prerna Paliwal <prernapaliwal@users.noreply.github.com>
2020-05-09 22:21:00 +0530

    committed

---
+++ analyzer.py
@@ -0,0 +1,132 @@
+import pandas as pd
+
+import datetime
+from datetime import datetime
+
+from youtube_api import YouTubeDataAPI
+import os
+import googleapiclient.discovery
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# import matplotlib.pyplot as plt
+
+import pycountry
+
+# settings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def data(region,category,video):
+    region = region
+    categoryId = category
+    video_name = video
+
+    country = pycountry.countries.get(name=region).alpha_2
+
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyAiv4C3h8Ei3hWmQyRYs6Gh-y5PxFjAa8k"
+
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+
+
+    # popular = youtube.videos().list(part="contentDetails,snippet,statistics",regionCode=country,chart="mostPopular",videoCategoryId=
+    #                                 categoryId).execute()
+
+
+    popular = []
+    view_max = 0
+    like_max = 0
+
+    request = youtube.search().list(q=video_name, part='id', maxResults=50, type='video').execute()
+    print(request)
+    for i in range(len(request)):
+        video_id = request['items'][i]['id']['videoId']
+        pop = youtube.videos().list(part="contentDetails,snippet,statistics", regionCode=country, id=video_id,
+                                    videoCategoryId=
+                                    categoryId).execute()
+        #     req = youtube.commentThreads().list(part="snippet",videoId=video_id,textFormat="plainText").execute()
+        print(pop)
+        view = int(pop['items'][0]['statistics']['viewCount'])
+        like = int(pop['items'][0]['statistics']['likeCount'])
+        if (view_max < view):
+            view_max = view
+            like_max = like
+            popular.append(pop)
+            title = pop['items'][0]['snippet']['title']
+            channel_title =  pop['items'][0]['snippet']['channelTitle']
+
+            video_id = pop['items'][0]['id']
+            # print(pop)
+            # print(channel_title)
+            word = pop['items'][0]['snippet']['description'].replace(" ", "")
+            d = list(x for x in word.split("\n"))
+
+            for x in d:
+                x = list(x.split('-'))
+                # print(x)
+                if (x[0] == 'YouTubeMusic' ):
+
+                    video = x[1]
+                    # print(video)
+
+
+                elif(x[0] == 'Gaana'):
+
+                    video = x[1]
+                    # print(x)
+                    # print('n',str(x))
+                else:
+                    video = d[0][1]
+                    # print(video)
+
+            video_link = video
+
+    req = youtube.commentThreads().list(part="snippet",videoId=video_id,textFormat="plainText").execute()
+    # print(req)
+    threads = []
+    comments = []
+    dates = []
+    likes = []
+    for item in req["items"]:
+        threads.append(item)
+        comment = item["snippet"]["topLevelComment"]
+        text = comment["snippet"]["textDisplay"]
+        date = comment["snippet"]['publishedAt']
+        like = comment["snippet"]['likeCount']
+
+        comments.append(text)
+        dates.append(date)
+        likes.append(like)
+
+    data = list(zip(comments, dates, likes))
+    data2 = pd.DataFrame(data)
+    data2.columns = ['comments', 'dates', 'likes']
+
+
+    sentiments = []
+    sid = SentimentIntensityAnalyzer()
+    scores = {}
+
+    for sentence in comments:
+        sentiment = sid.polarity_scores(sentence)
+
+        sentiments.append([sentence, sentiment['neg'], sentiment['pos'],
+                           sentiment['neu'], sentiment['compound']])
+
+    df = pd.DataFrame(sentiments)
+    df.columns = ['sentence', 'pos', 'neg', 'neu', 'compound']
+    total = df.sum(axis=0, skipna=True)
+
+
+
+    print(video_id)
+
+    return total,channel_title,video_id
+
+
+
+# data('India','Music','Teri Mitti')
---
+++ main.py
@@ -0,0 +1,55 @@
+
+from flask import Flask, request,jsonify, render_template
+import pandas as pd
+import json
+import plotly
+import plotly.graph_objs as go
+import analyzer
+
+import sys
+
+
+
+server = Flask(__name__)
+
+
+@server.route("/")
+def hello():
+
+   return render_template("cards.html")
+
+
+@server.route("/cards", methods=["GET","POST"])
+def predict():
+    video = request.form['search']
+    region = request.form['country']
+    category = request.form['Category']
+    try:
+        total,channel_title,video_id = (analyzer.data(region,category,video))
+        total = dict(total)
+        print(total)
+        # print(type(total))
+        render = "https://www.youtube.com/embed/"+video_id
+        print(render)
+        return render_template("dash.html", Positive=round(total['pos'],2),Negative=round(total['neg'],2),Neutral=round(total['neu'],2),channel = channel_title,videoID=render)
+    except Exception as e:
+        return render_template("error_404.html", error=str(e))
+
+
+@server.errorhandler(404)
+def page_not_found(e):
+    return render_template('error_404.html'), 404
+
+
+if __name__ == '__main__':
+    # try:
+    #     port = int(sys.argv[1]) # This is for a command-line input
+    # except:
+    #     port = 12345 # If you don't provide any port the port will be set to 12345
+
+    # lr = joblib.load("model.pkl") # Load "model.pkl"
+    # print ('Model loaded')
+    # model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    # print ('Model columns loaded')
+    # port=port,
+    server.run( debug=True)
